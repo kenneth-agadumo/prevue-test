25afc66d69c43ce4cf991849c43ca849b97f1b96
kenneth-agadumo <agadumok@gmail.com>
2025-02-19 22:05:40 +0100

    .debug

--- src/components/FeaturedTabs.jsx
+++ src/components/FeaturedTabs.jsx
@@ -278,8 +278,8 @@ export const ActivitiesSection = () => {
         <p className="tab-content-text  ">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           varius enim in eros elementum tristique. Duis cursus, mi quis viverra
-          ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.
-        </p>
+          ornare, eros nterdum nulla, ut commodo diam libero vitae erat.
+        </p> 
         <div className="content-row">
           {/* <Dropdown
             itemNumber={3}
